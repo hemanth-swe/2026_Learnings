a7fd381b4821ca00deef5e6643ffd1f84a50105d
hemanth-swe <hemanthkumarbudige@gmail.com>
2026-01-21 22:14:29 +0530

    routes

--- React/first_reactApp/src/App.jsx
+++ React/first_reactApp/src/App.jsx
@@ -1,38 +1,34 @@
-import { useState, memo } from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-//const Card = memo(function Card({ title }) {
-//   const [isLiked, setIsLiked] = useState(false);
+function Home() {
+  return <h1>Home Page</h1>;
+}
 
-  // return (
-    
-    // <div className="flex items-center justify-between p-4 m-4 border-2 border-black rounded-lg shadow-sm w-64">
-    //   <h3 className="text-lg font-semibold">{title}</h3>
+function About() {
+  return <h1>About Page</h1>;
+}
 
-    //   <button
-    //     onClick={() => setIsLiked(prev => !prev)}
-    //     aria-label={isLiked ? "Unlike" : "Like"}
-    //     className="text-2xl transition-transform hover:scale-110"
-    //   >
-    //     {isLiked ? "❤️" : "🤍"}
-    //   </button>
-    // </div>
-    
+function Contact() {
+  return <h1>Contact Page</h1>;
+}
 
-//   );
-// });
-
-export default function App() {
-  const movies = ["movie_1", "movie_2", "movie_3"];
-
+function App() {
   return (
-    // <div className="flex gap-4 flex-wrap p-6">
-    //   {movies.map(movie => (
-    //     <Card key={movie} title={movie} />
-    //   ))}
-    // </div>
-    <main>
-      
-        <h3 className="text-lg font-semibold">FIND NEW <span className="text-gradient">MOVIES</span></h3>
-    </main>
+    <BrowserRouter>
+      {/* Navigation */}
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+export default App;
